bb2b0def40557d19debc287edc8f82910bc6e5ec
vedant vasaikar <ninadvasaikar@gmail.com>
2024-11-04 12:01:14 +0530

    vercel settings

--- usermodel_withextra/wsgi.py
+++ usermodel_withextra/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from usermodel_withextra.wsgi import application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usermodel_withextra.settings')
 
